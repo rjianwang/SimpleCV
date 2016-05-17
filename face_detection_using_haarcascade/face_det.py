a759be85dd5d7b875ca454d889399e39241493dc
rjianwang <rjianwang@foxmail.com>
2016-05-17 10:49:55 +0800

    Face Detection Using Haarcascade

--- face_detection_using_haarcascade/face_det.py
+++ face_detection_using_haarcascade/face_det.py
@@ -6,14 +6,16 @@ import cv2
 def face_det(img):
     # convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.equalizeHist(gray, gray)
 
     # detect faces
     cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
     faces = cascade.detectMultiScale(gray, 1.3, 5)
 
     # draw rectangles around faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+    if len(faces) > 0:
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
 
     # show results
     cv2.imshow('Face Detection', img)
@@ -24,5 +26,8 @@ def face_det(img):
 
 if __name__ == '__main__':
     img = cv2.imread('lena.jpg')
+    #img = cv2.imread('liushishi.jpg')
+    #img = cv2.imread('liushishi2.jpg')
+    #img = cv2.imread('liushishi3.jpg')
     face_det(img)
 
